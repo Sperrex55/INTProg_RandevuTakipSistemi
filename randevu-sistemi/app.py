613fe2877ccfa0b9fed760d89983f186269e3a4c
arda ırmak <132782431+Sperrex55@users.noreply.github.com>
2025-05-23 15:30:30 +0300

    Update app.py

--- randevu-sistemi/app.py
+++ randevu-sistemi/app.py
@@ -450,8 +450,6 @@ def cikis():
  #   with app.app_context():
   #      db.create_all()
    # socketio.run(app, debug=True)
-
 import os
-if __name__ ** "__main__":
-    app.run(host="0.0.0.0" , port=int(os.environ.get("PORT", 5000)))
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
